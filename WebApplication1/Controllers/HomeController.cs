8109547fabb7aa4dbcaccc461859ea1801431216
Deva9x <daalpiva@gmail.com>
2019-07-15 21:47:43 -0500

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepositorioPais Repositorio { get; }
+
+        public HomeController(IRepositorioPais Repositorio) {
+            this.Repositorio = Repositorio;
+        }
+
+        public IActionResult Index()
+        {
+            List<Pais> paises = Repositorio.obtenerPaises();
+            return View(paises);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/ViewComponents/PaisesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Services;
+
+namespace WebApplication1.ViewComponents
+{
+    public class PaisesViewComponent : ViewComponent
+    {
+        public PaisesViewComponent(IRepositorioPais repositorioPais) {
+            RepositorioPais = repositorioPais;
+        }
+
+        public IRepositorioPais RepositorioPais { get; }
+
+        public IViewComponentResult Invoke() {
+            var paises = RepositorioPais.obtenerPaises();
+            return View(paises);
+        }
+    }
+}
